312bc5a6d97c9751faaab4879a588d328dfc4888
t-bast <bastuc@hotmail.fr>
2017-09-01 13:18:24 +0200

    Add a command-line argument parser

--- RedBanana/app/Main.hs
+++ RedBanana/app/Main.hs
@@ -1,6 +1,31 @@
 module Main where
 
-import Lib
+import Options.Applicative
+import Data.Semigroup ((<>))
 
+data Args = Args
+    { address      :: String
+    , verbose      :: Bool }
+
+args :: Parser Args
+args = Args
+        <$> argument str
+            ( metavar "address"
+            <> help "Address to analyze" )
+        <*> switch
+            ( long "verbose"
+            <> short 'v'
+            <> help "Output verbosity" )
+        
 main :: IO ()
-main = someFunc
+main =
+    analyze =<< execParser opts
+        where
+        opts = info (args <**> helper)
+            ( fullDesc
+            <> progDesc "Analyze transaction to an ethereum address"
+            <> header "Red Banana" )
+    
+analyze :: Args -> IO ()
+analyze (Args h False) = putStrLn $ "Analyzing, " ++ h
+analyze _ = return ()
