74b4a2d59d7fa5c8554d3bf335e63868042f991e
t-bast <bastuc@hotmail.fr>
2017-09-06 17:29:21 +0200

    Tweak the ethereum-client

--- RedBanana/src/RedBanana/EthereumClient.hs
+++ RedBanana/src/RedBanana/EthereumClient.hs
@@ -1,18 +1,14 @@
 module RedBanana.EthereumClient where
     
 import Control.Lens
-import Data.Aeson (Value)
+import Data.Aeson (fromJSON, Result)
 import Data.Aeson.Lens (key)
--- import Data.ByteString
 import Data.Text
 import Network.Wreq
 import RedBanana.Types
 
--- getTransactions :: Text -> IO (Response [Transaction])
--- getTransactions address = do
---     let query = defaults & param "address" .~ [address]
---     r <- getWith query "http://api.etherscan.io/api?module=account&action=txlist&startblock=0&endblock=99999999&sort=asc"
---     asJSON =<< r ^? responseBody . key "result"
-    -- r ^? responseBody . key "result"
-    -- return (fromJSON (r ^. responseBody . key "result"))
-    -- asJSON =<< r ^. responseBody . key "result"
+getTransactions :: Text -> IO (Maybe (Result [Transaction]))
+getTransactions address = do
+    let query = defaults & param "address" .~ [address]
+    r <- getWith query "http://api.etherscan.io/api?module=account&action=txlist&startblock=0&endblock=99999999&sort=asc"
+    return $ fmap fromJSON $ r ^? responseBody . key "result" 
