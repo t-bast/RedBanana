92817861cdfcc9891f0e9c27d2f6bef02473b775
Jérémy Derdaele <jeremy.derdaele@gmail.com>
2017-09-08 16:54:24 +0200

    Use Aeson parsers to parse Transaction

--- RedBanana/src/RedBanana/Etherscan.hs
+++ RedBanana/src/RedBanana/Etherscan.hs
@@ -1,27 +1,30 @@
 module RedBanana.Etherscan (getTransactions) where
-    
-import Control.Lens ((^.), (.~), (&), (^?))    
-import Data.Aeson (Value)
+
+import Control.Lens ((^.), (.~), (&), (^?))
+import Data.Aeson (Value(..), (.:))
 import Data.Aeson.Lens (key, _Integer, _String, _Array)
+import Data.Aeson.Types (Parser, parseMaybe, parse, Result(..))
 import Data.Text (Text)
 import Network.Wreq
 import Data.Vector (toList)
 import RedBanana.Types
 
-toTransaction :: Value -> Maybe Transaction
-toTransaction val = Transaction 
-    <$> val ^? key "blockHash" . _String
-    <*> val ^? key "blockNumber" . _Integer
-    <*> val ^? key "confirmations" . _Integer
-    <*> val ^? key "from" . _String
-    <*> val ^? key "to" . _String
-    <*> val ^? key "value" . _Integer
+asInt :: String -> Integer
+asInt = read
+
+toTransaction :: Value -> Parser Transaction
+toTransaction (Object v) =
+    Transaction
+    <$> v .: "blockHash"
+    <*> (asInt <$> v .: "blockNumber")
+    <*> (asInt <$> v .: "confirmations")
+    <*> v .: "from"
+    <*> v .: "to"
+    <*> (asInt <$> v .: "value")
 
 getTransactions :: Text -> IO (Maybe [Transaction])
 getTransactions address = do
     let query = defaults & param "address" .~ [address]
-    r <- getWith query "http://api.etherscan.io/api?module=account&action=txlist&startblock=0&endblock=10&sort=asc"
-    let tx = r ^? responseBody . key "result" . _Array
-    return $ case tx of 
-        Nothing -> Nothing
-        Just xs -> mapM toTransaction $ toList xs
+    r <- getWith query "http://api.etherscan.io/api?module=account&action=txlist&startblock=0&endblock=99999999&sort=asc"
+    let tx = r ^. responseBody . key "result" . _Array
+    return $ mapM (parseMaybe toTransaction) $ toList tx
