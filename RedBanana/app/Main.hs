f5960598b2ad578b0c88c043f2bd17b28ebd6bca
t-bast <bastuc@hotmail.fr>
2017-09-26 14:40:50 +0200

    Add more command-line arguments

--- RedBanana/app/Main.hs
+++ RedBanana/app/Main.hs
@@ -3,12 +3,17 @@ module Main where
 import RedBanana.Etherscan
 import RedBanana.GraphGenerator
 
+import Control.Monad
+import Data.Semigroup ((<>))
 import qualified Data.Text as T
 import Options.Applicative
-import Data.Semigroup ((<>))
 
 data Args = Args
     { address      :: String
+    , blockChain   :: String
+    , depth        :: Int
+    , startBlock   :: Int
+    , endBlock     :: Int
     , verbose      :: Bool }
 
 args :: Parser Args
@@ -16,6 +21,34 @@ args = Args
         <$> argument str
             ( metavar "address"
             <> help "Address to analyze" )
+        <*> strOption
+            ( long "blockchain"
+            <> short 'b'
+            <> metavar "string"
+            <> help "Blockchain to analyze"
+            <> showDefault
+            <> value "ethereum")
+        <*> option auto
+            ( long "depth"
+            <> short 'd'
+            <> metavar "int"
+            <> help "Depth of the transactions graph"
+            <> showDefault
+            <> value 1)
+        <*> option auto
+            ( long "startBlock"
+            <> short 's'
+            <> metavar "int"
+            <> help "Start block"
+            <> showDefault
+            <> value 0)
+        <*> option auto
+            ( long "endBlock"
+            <> short 'e'
+            <> metavar "int"
+            <> help "End block"
+            <> showDefault
+            <> value 99999999)
         <*> switch
             ( long "verbose"
             <> short 'v'
@@ -27,15 +60,15 @@ main =
         where
         opts = info (args <**> helper)
             ( fullDesc
-            <> progDesc "Analyze transaction to an ethereum address"
+            <> progDesc "Analyze transaction flow made to a blockchain address"
             <> header "Red Banana" )
 
 analyze :: Args -> IO ()
-analyze (Args addr False) = do
-    let depth = 2
-    putStrLn $ "Searching for transactions sent to " ++ addr
-    putStrLn $ "Maximum graph depth: " ++ show depth
-    tx <- getTransactionsFlow (T.pack addr) 0 99999999 "asc" depth
-    putStrLn $ (show . length $ tx) ++ " transactions found"
+analyze (Args addr "ethereum" depth startBlock endBlock verbose) = do
+    when verbose $ putStrLn $ "Searching for transactions sent to " ++ addr
+    when verbose $ putStrLn $ "Maximum graph depth: " ++ show depth
+    tx <- getTransactionsFlow (T.pack addr) startBlock endBlock "asc" depth
+    when verbose $ putStrLn $ (show . length $ tx) ++ " transactions found"
     putStr $ T.unpack . generate $ tx
-analyze _ = return ()
+analyze (Args _ blockchain _ _ _ _) = 
+    putStrLn $ "Analysis of " ++ blockchain ++ " blockchain is not supported yet. Try RedBanana-exe --help for help."
